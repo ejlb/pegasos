0e0da792b7b70d7ea054e72aa2183eae2d126e86
eddie bell <eddie@lyst.com>
2013-05-16 11:26:06 +0100

    one against all example with svmlight data

--- example/ovr_example.py
+++ example/ovr_example.py
@@ -6,39 +6,45 @@ import sys
 from sklearn.cross_validation import KFold
 from sklearn.grid_search import IterGrid
 
-def _remap_labels(label_map, label):
-    new_label_map = {}
-    for k,v in label_map.items():
-        if v == label:
-            new_label_map[k] = 1.0
-        else:
-            new_label_map[k] = -1.0
+def _remap_labels(data, label):
+    label_map = {}
 
-    return new_label_map
+    for i in range(0, data.NumExamples()):
+        y = data.VectorAt(i).GetY()
+        label_map[y] = 1.0 if label == y else -1.0
 
-def _remap_data(remapped_labels, data):
-    #### use remapped_labels to build a new sf-dataset vi sf-spare-vector
-    return data
+    return label_map
+
+def _remap_data(label_map, data):
+    remapped_data = sofia.SfDataSet(True)
+
+    for i in range(0, data.NumExamples()):
+        sparse_vector = data.VectorAt(i)
+        remapped_data.AddLabeledVector(sparse_vector,
+                                       label_map[sparse_vector.GetY()])
+    return remapped_data
 
 def one_vs_all(train, test, config):
     train_sf = sofia.SfDataSet(''.join(train), True)
     test_sf = sofia.SfDataSet(''.join(test), True)
 
-    label_map = dict([(i, train_sf.VectorAt(i).GetY())
-        for i in range(train_sf.NumExamples())])
-    label_set = set(label_map.values())
+    label_set = set([train_sf.VectorAt(i).GetY()
+            for i in range(train_sf.NumExamples())])
 
     w = []
     error = []
 
-    ## re-map labels for 1-vs-all and average error
     for label in label_set:
-        remapped_labels = _remap_labels(label_map, label)
-        remapped_train = _remap_data(remapped_labels, train_sf)
-        remapped_test = _remap_data(remapped_labels, test_sf)
+        label_map = _remap_labels(train_sf, label)
+        remapped_train = _remap_data(label_map, train_sf)
+        remapped_test = _remap_data(label_map, test_sf)
 
         w.append(sofia.TrainModel(remapped_train, config))
         error.append(sofia.sofia_ml.SvmObjective(remapped_test, w[-1], config))
+
+        # don't need 1-v-all for the binary labels
+        if len(label_set) == 2:
+            break
 
     return error, w
 
@@ -57,15 +63,15 @@ print 'got %d data-points' % len(data)
 config = sofia.sofia_ml.SofiaConfig()
 config.learner_type = sofia.sofia_ml.LOGREG_PEGASOS;
 config.loop_type = sofia.sofia_ml.BALANCED_STOCHASTIC;
-config.prediction_type = sofia.sofia_ml.LOGISTIC;
+config.prediction_type = sofia.sofia_ml.LINEAR;
 
 sofia.srand(31337)
 
 param_errors = {}
-cv = 10
+cv = 2
 
 param_grid = {
-    'lambda' : [0.00001, 0.0001, 0.001, 0.01, 0.1],
+    'lambda' : [0.0001, 0.001, 0.01, 0.1],
 }
 
 for params in IterGrid(param_grid):
