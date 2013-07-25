856acadafe9aafd4eb232bbba1a99ee9ee2c3b50
Eddie Bell <ejlbell@gmail.com>
2013-07-25 20:17:50 +0100

    remove old tests

--- tests/pegasos_tests.py
+++ tests/pegasos_tests.py
@@ -3,7 +3,9 @@ from sklearn.datasets import load_digits
 
 import pegasos
 
-def test_setup():
-    pegasos.PegasosSVMClassifier()
-    pegasos.PegasosLogisticRegression()
-    return False
+def test_init():
+    try:
+        pegasos.PegasosSVMClassifier()
+        pegasos.PegasosLogisticRegression()
+    except:
+        assert False, 'Failed to initialise classifiers'
