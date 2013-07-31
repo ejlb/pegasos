92ab24a3dc5e943bf5d2ff650ebdbdac64314b13
eddie bell <eddie@lyst.com>
2013-07-31 11:27:00 +0100

    add apache 2.0 license

--- pegasos/pegasos.py
+++ pegasos/pegasos.py
@@ -1,7 +1,23 @@
 """
-Functions for fitting and predicting. Heavily inspired
-by google's C++ sofia-ml implementation.
+    Copyright 2013 Lyst Ltd.
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
 """
+
+
+# Functions for fitting and predicting. Heavily inspired by google's C++
+# sofia-ml implementation.
+
 
 import random
 import math
